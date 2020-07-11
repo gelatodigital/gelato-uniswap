6d516fccb31ca341620dedd0ac11673a2f5f1666
Hilmar Orth <hm.orth@aol.com>
2020-07-11 18:10:48 +0200

    imported gelatonetwork/core library

--- contracts/gelato_conditions/GelatoConditionsStandard.sol
+++
@@ -1,8 +0,0 @@
-// "SPDX-License-Identifier: UNLICENSED"
-pragma solidity ^0.6.10;
-
-import "./IGelatoCondition.sol";
-
-abstract contract GelatoConditionsStandard is IGelatoCondition {
-    string internal constant OK = "OK";
-}
